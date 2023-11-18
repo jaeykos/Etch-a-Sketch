c5832f5b9eb32372336ba227329dd453940636b8
Jae Kang <jaeykos@gmail.com>
2023-11-18 09:12:59 -0700

    various udpates; chnage background & font; prevent dragging

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-setTimeout(function () {window.scrollTo(0, 0);},200); //scroll to top-left when page loads; timeout set to delay; othewise doesn't work
+setTimeout(function () {
+  window.scrollTo(0, 0);
+}, 200); //scroll to top-left when page loads; timeout set to delay; othewise doesn't work
 
 //colors for rgb mode
-const colors = ['#ff6666', '#ff9966','#cccc00', '#33ff99', '#80dfff','#9999ff','#cc99ff' ]
-let colorIndex =-1
+const colors = [
+  "#ff6666",
+  "#ff9966",
+  "#cccc00",
+  "#33ff99",
+  "#80dfff",
+  "#9999ff",
+  "#cc99ff",
+];
+let colorIndex = -1;
 
 //default on/off boolean values
 let isMouseLeftDown = false;
@@ -11,190 +21,215 @@ let isBorderColorPickerOn = false;
 let isBackgroundColorPickerOn = false;
 let isRgbModeOn = false;
 let isEraserModeOn = false;
-let backgroundSelectedColor = "grey";
-let borderSelectedColor = "white" ;
-let paintSelectedColor = "rgb(90, 90, 90)"; 
+let backgroundSelectedColor = "white";
+let borderSelectedColor = "black";
+let paintSelectedColor = "rgb(182, 182, 182)";
 
 //populate drawing blocks in drawing board
 let dwgBlockSize = 50;
-resetDwgBoardWithPixelSize()
+resetDwgBoardWithPixelSize();
+paintColorButton.style.backgroundColor = "rgb(182, 182, 182)";
 
-document.addEventListener("click", e =>{
-    backgroundColorPickerDiv.style.display = "none";
-    borderColorPickerDiv.style.display = "none";
-    paintColorPickerDiv.style.display = "none";
+document.addEventListener("click", (e) => {
+  backgroundColorPickerDiv.style.display = "none";
+  borderColorPickerDiv.style.display = "none";
+  paintColorPickerDiv.style.display = "none";
 
-    if(e.target.id != "pixelSlider" && e.target.id != "pixelButton"){
-        pixelSlider.style.display = "none";
-    }
+  if (
+    e.target.id != "pixelSlider" &&
+    e.target.id != "pixelButton" &&
+    e.target.id != "pixelSliderRange"
+  ) {
+    pixelSlider.style.display = "none";
+  }
 
-    if (e.target.id == "pixelButton"){clickPixelButton(e); return;}
-    if (e.target.id == "backgroundColorButton"){clickBackgroundColorButton(e); return;}
-    if (e.target.id == "borderColorButton"){clickBorderColorButton(e); return;}
-    if (e.target.id == "paintColorButton"){clickPaintColorButton(e); return;}
-    if (e.target.id == "rgbButton"){clickRgbButton(e);return;}
-    if (e.target.id == "eraserButton"){clickEraserButton(e);return;}
-    if (e.target.id == "clearButton"){clickClearButton(e);return;}
+  if (e.target.id == "pixelButton") {
+    clickPixelButton(e);
+    return;
+  }
+  if (e.target.id == "backgroundColorButton") {
+    clickBackgroundColorButton(e);
+    return;
+  }
+  if (e.target.id == "borderColorButton") {
+    clickBorderColorButton(e);
+    return;
+  }
+  if (e.target.id == "paintColorButton") {
+    clickPaintColorButton(e);
+    return;
+  }
+  if (e.target.id == "rgbButton") {
+    clickRgbButton(e);
+    return;
+  }
+  if (e.target.id == "eraserButton") {
+    clickEraserButton(e);
+    return;
+  }
+  if (e.target.id == "clearButton") {
+    clickClearButton(e);
+    return;
+  }
 });
 
-function clickEraserButton(){
-    if (isEraserModeOn == false){
-        isEraserModeOn = true;
-        eraserButton.style.opacity = "30%";
-        return;
-    }
-    else if(isEraserModeOn == true){
-        isEraserModeOn = false;
-        eraserButton.style.opacity = '100%';
-        return;
-    }
+function clickEraserButton() {
+  if (isEraserModeOn == false) {
+    isEraserModeOn = true;
+    eraserButton.style.opacity = "30%";
+    return;
+  } else if (isEraserModeOn == true) {
+    isEraserModeOn = false;
+    eraserButton.style.opacity = "100%";
+    return;
+  }
 }
 
-function clickClearButton(e){
-    
-    resetDwgBoardWithPixelSize()
+function clickClearButton(e) {
+  resetDwgBoardWithPixelSize();
 }
 
-function clickRgbButton(){
-    if (isRgbModeOn == false){
-        isRgbModeOn = true;
-        rgbButton.style.background = "linear-gradient(90deg,"+
-            "rgba(255, 0, 0, 1) 0%,"+
-            "rgba(255, 154, 0, 1) 10%,"+
-            "rgba(208, 222, 33, 1) 20%,"+
-            "rgba(79, 220, 74, 1) 30%,"+
-            "rgba(63, 218, 216, 1) 40%,"+
-            "rgba(47, 201, 226, 1) 50%,"+
-            "rgba(28, 127, 238, 1) 60%,"+
-            "rgba(95, 21, 242, 1) 70%,"+
-            "rgba(186, 12, 248, 1) 80%,"+
-            "rgba(251, 7, 217, 1) 90%,"+
-            "rgba(255, 0, 0, 1) 100%";
-        return;
-    }
-    else if(isRgbModeOn == true){
-        isRgbModeOn = false;
-        rgbButton.style.background = '';
-        return;
-    }
-}  
+function clickRgbButton() {
+  if (isRgbModeOn == false) {
+    isRgbModeOn = true;
+    rgbButton.style.background =
+      "linear-gradient(90deg," +
+      "rgba(255, 0, 0, 1) 0%," +
+      "rgba(255, 154, 0, 1) 10%," +
+      "rgba(208, 222, 33, 1) 20%," +
+      "rgba(79, 220, 74, 1) 30%," +
+      "rgba(63, 218, 216, 1) 40%," +
+      "rgba(47, 201, 226, 1) 50%," +
+      "rgba(28, 127, 238, 1) 60%," +
+      "rgba(95, 21, 242, 1) 70%," +
+      "rgba(186, 12, 248, 1) 80%," +
+      "rgba(251, 7, 217, 1) 90%," +
+      "rgba(255, 0, 0, 1) 100%";
+    return;
+  } else if (isRgbModeOn == true) {
+    isRgbModeOn = false;
+    rgbButton.style.background = "";
+    return;
+  }
+}
 
 function clickPixelButton(e) {
-    if (pixelSlider.style.display == "none"){
-        pixelSlider.style.display = "flex";
-    }else{
-        pixelSlider.style.display = "none";
+  if (pixelSlider.style.display == "none") {
+    pixelSlider.style.display = "flex";
+  } else {
+    pixelSlider.style.display = "none";
+  }
+}
+
+function clickPaintColorButton(e) {
+  paintColorPickerDiv.style.display = "flex";
+  paintColorPicker.focus();
+  paintColorPicker.click();
+  isPaintColorPickerOn = true;
+}
+
+function clickBackgroundColorButton(e) {
+  backgroundColorPickerDiv.style.display = "flex";
+  backgroundColorPicker.focus();
+  backgroundColorPicker.click();
+  isBackgroundColorPickerOn = true;
+}
+
+function clickBorderColorButton(e) {
+  borderColorPickerDiv.style.display = "flex";
+  borderColorPicker.focus();
+  borderColorPicker.click();
+  isBorderColorPickerOn = true;
+}
+
+borderColorPicker.oninput = function () {
+  borderSelectedColor = this.value;
+  borderColorButton.style.backgroundColor = borderSelectedColor;
+  resetDwgBoardWithPixelSize();
+};
+
+backgroundColorPicker.oninput = function () {
+  backgroundSelectedColor = this.value;
+  backgroundColorButton.style.backgroundColor = backgroundSelectedColor;
+  resetDwgBoardWithPixelSize();
+};
+
+paintColorPicker.oninput = function () {
+  paintSelectedColor = this.value;
+  paintColorButton.style.backgroundColor = paintSelectedColor;
+};
+
+document.addEventListener("mousedown", (e) => {
+  if (e.target.classList == "dwgBlock") {
+    e.preventDefault();
+    isMouseLeftDown = true;
+    if (isEraserModeOn == true) {
+      e.target.style.backgroundColor = backgroundSelectedColor;
+    } else if (isRgbModeOn == true) {
+      if (colorIndex >= colors.length - 1) {
+        colorIndex = 0;
+      } else {
+        colorIndex++;
+      }
+      e.target.style.backgroundColor = colors[colorIndex];
+    } else if (isRgbModeOn == false) {
+      e.target.style.backgroundColor = paintSelectedColor;
     }
-}
-
-function clickPaintColorButton(e){
-    paintColorPickerDiv.style.display = "flex";
-    paintColorPicker.focus();
-    paintColorPicker.click();
-    isPaintColorPickerOn = true
-}
-
-function clickBackgroundColorButton(e){
-    backgroundColorPickerDiv.style.display = "flex";
-    backgroundColorPicker.focus();
-    backgroundColorPicker.click();
-    isBackgroundColorPickerOn = true
-}
-
-function clickBorderColorButton(e){
-    borderColorPickerDiv.style.display = "flex";
-    borderColorPicker.focus();
-    borderColorPicker.click();
-    isBorderColorPickerOn = true
-}
-
-borderColorPicker.oninput = function() {
-    borderSelectedColor = this.value;
-    borderColorButton.style.backgroundColor = borderSelectedColor;
-    resetDwgBoardWithPixelSize()
-} 
-
-backgroundColorPicker.oninput = function() {
-    backgroundSelectedColor = this.value;
-    backgroundColorButton.style.backgroundColor = backgroundSelectedColor;
-    resetDwgBoardWithPixelSize()
-} 
-
-paintColorPicker.oninput = function() {
-    paintSelectedColor = this.value;
-    paintColorButton.style.backgroundColor = paintSelectedColor;
-} 
-
-document.addEventListener("mousedown", e => {
-    if (e.target.classList== "dwgBlock"){
-        isMouseLeftDown = true;
-        if(isEraserModeOn == true){
-            e.target.style.backgroundColor = backgroundSelectedColor ;
-        } else if (isRgbModeOn == true){
-            if (colorIndex >= colors.length -1){
-                colorIndex = 0 
-            }else{
-                colorIndex++
-            }
-            e.target.style.backgroundColor = colors[colorIndex];
-        }else if (isRgbModeOn == false){
-            e.target.style.backgroundColor = paintSelectedColor ;
-        }   
-    }
+  }
 });
 
 addEventListener("mouseover", (e) => {
-    if (e.target.classList == "dwgBlock" && isMouseLeftDown == true){
-        if(isEraserModeOn == true){
-            e.target.style.backgroundColor = backgroundSelectedColor ;
-        } else if (isRgbModeOn == true){
-            if (colorIndex >= colors.length -1){
-                colorIndex = 0 
-            }else{
-                colorIndex++
-            }
-            e.target.style.backgroundColor = colors[colorIndex];
-        }else if (isRgbModeOn == false){
-            e.target.style.backgroundColor = paintSelectedColor ;
-        }   
-    } 
-})
-
-document.addEventListener("mouseup", (event) => {
-    isMouseLeftDown = false;
+  if (e.target.classList == "dwgBlock" && isMouseLeftDown == true) {
+    if (isEraserModeOn == true) {
+      e.target.style.backgroundColor = backgroundSelectedColor;
+    } else if (isRgbModeOn == true) {
+      if (colorIndex >= colors.length - 1) {
+        colorIndex = 0;
+      } else {
+        colorIndex++;
+      }
+      e.target.style.backgroundColor = colors[colorIndex];
+    } else if (isRgbModeOn == false) {
+      e.target.style.backgroundColor = paintSelectedColor;
+    }
+  }
 });
 
-pixelSliderRange.oninput = function() {
-    dwgBlockSize = this.value;
-    resetDwgBoardWithPixelSize();
-  }
+document.addEventListener("mouseup", (event) => {
+  isMouseLeftDown = false;
+});
 
-function resetDwgBoardWithPixelSize(){
-    pixelButton.innerHTML = "Pixel Size: " + dwgBlockSize+ "px";
+pixelSliderRange.oninput = function () {
+  dwgBlockSize = this.value;
+  resetDwgBoardWithPixelSize();
+};
 
-    dwgBoard.innerHTML = '';
+function resetDwgBoardWithPixelSize() {
+  pixelButton.innerHTML = "Pixel Size: " + dwgBlockSize + "px";
 
-    dwgBoardWidth = window.innerWidth - 300;
-    dwgBoardHeight = window.innerHeight;
+  dwgBoard.innerHTML = "";
 
-    dwgBoard.style.width = dwgBoardWidth.toString();
+  dwgBoardWidth = window.innerWidth - 300;
+  dwgBoardHeight = window.innerHeight;
 
-    numColumns = dwgBoardWidth / dwgBlockSize;
-    numRows = dwgBoardHeight /dwgBlockSize; 
+  dwgBoard.style.width = dwgBoardWidth.toString();
 
-    for (let i = 0; i < (numColumns); i++){
-        const dwgRow = document.createElement("span");
-        dwgRow.id = "Row" + i;
-        dwgRow.classList.add("dwgRow");
-        document.getElementById('dwgBoard').appendChild(dwgRow);  
-        for(let j = 0; j< numRows; j++){
-            const element = document.createElement("div");
-            element.style.width = (dwgBlockSize +"px");
-            element.style.height = (dwgBlockSize +"px");
-            element.style.backgroundColor = backgroundSelectedColor;
-            element.style.borderColor = borderSelectedColor;
-            element.classList.add("dwgBlock");
-            document.getElementById(dwgRow.id).appendChild(element);
-        }  
+  numColumns = dwgBoardWidth / dwgBlockSize;
+  numRows = dwgBoardHeight / dwgBlockSize;
+
+  for (let i = 0; i < numColumns; i++) {
+    const dwgRow = document.createElement("span");
+    dwgRow.id = "Row" + i;
+    dwgRow.classList.add("dwgRow");
+    document.getElementById("dwgBoard").appendChild(dwgRow);
+    for (let j = 0; j < numRows; j++) {
+      const element = document.createElement("div");
+      element.style.width = dwgBlockSize + "px";
+      element.style.height = dwgBlockSize + "px";
+      element.style.backgroundColor = backgroundSelectedColor;
+      element.style.borderColor = borderSelectedColor;
+      element.classList.add("dwgBlock");
+      document.getElementById(dwgRow.id).appendChild(element);
     }
+  }
 }
